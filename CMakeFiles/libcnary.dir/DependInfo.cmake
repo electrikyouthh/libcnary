0bf69af9fa6cac2e31cbfc8cffdb07f3785f40d3
Electrik Youth <electrikyouthx@hotmail.com>
2023-06-05 09:50:19 -0700

    .

---
+++ CMakeFiles/libcnary.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/electrik/Documents/GitHub/libcnary/iterator.c" "CMakeFiles/libcnary.dir/iterator.c.o" "gcc" "CMakeFiles/libcnary.dir/iterator.c.o.d"
+  "/Users/electrik/Documents/GitHub/libcnary/list.c" "CMakeFiles/libcnary.dir/list.c.o" "gcc" "CMakeFiles/libcnary.dir/list.c.o.d"
+  "/Users/electrik/Documents/GitHub/libcnary/node.c" "CMakeFiles/libcnary.dir/node.c.o" "gcc" "CMakeFiles/libcnary.dir/node.c.o.d"
+  "/Users/electrik/Documents/GitHub/libcnary/node_iterator.c" "CMakeFiles/libcnary.dir/node_iterator.c.o" "gcc" "CMakeFiles/libcnary.dir/node_iterator.c.o.d"
+  "/Users/electrik/Documents/GitHub/libcnary/node_list.c" "CMakeFiles/libcnary.dir/node_list.c.o" "gcc" "CMakeFiles/libcnary.dir/node_list.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/libcnary.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/libcnary.dir/iterator.c.o"
+  "CMakeFiles/libcnary.dir/iterator.c.o.d"
+  "CMakeFiles/libcnary.dir/list.c.o"
+  "CMakeFiles/libcnary.dir/list.c.o.d"
+  "CMakeFiles/libcnary.dir/node.c.o"
+  "CMakeFiles/libcnary.dir/node.c.o.d"
+  "CMakeFiles/libcnary.dir/node_iterator.c.o"
+  "CMakeFiles/libcnary.dir/node_iterator.c.o.d"
+  "CMakeFiles/libcnary.dir/node_list.c.o"
+  "CMakeFiles/libcnary.dir/node_list.c.o.d"
+  "libcnary.a"
+  "libcnary.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/libcnary.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
